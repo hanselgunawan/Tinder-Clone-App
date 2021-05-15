c2586aae65b4657c45f382ccdab3bf8492e13bc6
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-05-15 00:15:46 -0700

    added swipe card & firebase dependencies

--- app/src/main/java/com/hanseltritama/tindercloneapp/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/tindercloneapp/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.hanseltritama.tindercloneapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.lorentzos.flingswipe.SwipeFlingAdapterView
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var al: ArrayList<String> = arrayListOf()
+    private var arrayAdapter: ArrayAdapter<String>? = null
+    private var i = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        al.add("php")
+        al.add("c")
+        al.add("python")
+        al.add("java")
+        al.add("html")
+        al.add("c++")
+        al.add("css")
+        al.add("javascript")
+
+        arrayAdapter = ArrayAdapter(this, R.layout.item, R.id.helloText, al)
+
+        val flingContainer: SwipeFlingAdapterView? = frame
+
+        flingContainer?.adapter = arrayAdapter
+        flingContainer?.setFlingListener(object : SwipeFlingAdapterView.onFlingListener {
+            override fun removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!")
+                al.removeAt(0)
+                arrayAdapter?.notifyDataSetChanged()
+            }
+
+            override fun onLeftCardExit(dataObject: Any) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                Toast.makeText(this@MainActivity, "Left!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onRightCardExit(dataObject: Any) {
+                Toast.makeText(this@MainActivity, "Right!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAdapterAboutToEmpty(itemsInAdapter: Int) {
+                // Ask for more data here
+                al.add("XML $i")
+                arrayAdapter!!.notifyDataSetChanged()
+                Log.d("LIST", "notified")
+                i++
+            }
+
+            override fun onScroll(scrollProgressPercent: Float) {
+            }
+        })
     }
 }
